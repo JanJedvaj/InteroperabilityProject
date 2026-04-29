12f628c788445188d5cb5e57080e8e8a0c4d1f13
JanJedvaj <jan.jedvaj2@gmail.com>
2026-04-29 17:55:47 +0200

    Initial folder structure

---
+++ src/test/java/hr/algebra/books/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.algebra.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Sanity check: Spring application context starts up cleanly.
+    }
+}
